eef99baab130f8adafac5519cb824d42fc2da409
punit144 <punit144@gmail.com>
2019-11-09 14:07:45 +0530

    Best Index Program

--- Hacker-Earth/best_index.py
+++ Hacker-Earth/best_index.py
@@ -4,4 +4,19 @@
 """
 ELEMENT = int(input())
 ARRAY = list(map(int, input().split()))
-print(ARRAY)
+
+ARRAY.append(0)
+
+#Variable Declaration
+CTR = 0
+END_CTR = 2
+I_SUM = ARRAY[ELMENT-2]
+MAX_SUM = 0
+# print(ARRAY)
+for i in range(-3, -ELEMENT-2, -1):
+    if CTR == END_CTR:
+        CTR = 0
+        END_CTR += 1
+    
+    ARRAY[i] = ARRAY[i+1]
+
