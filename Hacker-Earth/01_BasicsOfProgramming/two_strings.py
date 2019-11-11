cf670e0ec386c7b8ed6d7cfdf180c181222aae43
Punit Grewal <pgrewal@learn.localdomain>
2019-11-12 05:14:08 +0530

    New Problem Solved

--- Hacker-Earth/01_BasicsOfProgramming/two_strings.py
+++ Hacker-Earth/01_BasicsOfProgramming/two_strings.py
@@ -5,30 +5,34 @@
     Problem: HackerEarth <
     Complexity:
 """
+
+
 def permute(arrA, arrB):
     str_dic = {}
-    length = len(arrA)
+    value = True
     for lp_f1 in arrA:
         if lp_f1 in str_dic:
             str_dic[lp_f1] += 1
         else:
-            str_dic[lp_f1] = 0
+            str_dic[lp_f1] = 1
     for lp_f2 in arrB:
         if lp_f2 in str_dic:
             str_dic[lp_f2] -= 1
         else:
-            return("No")
-    if str_dic.values <= 0:
-        return("No")
+            pass
+            # value = False
+    for str_val in str_dic.values():
+        if str_val <= 0:
+            continue
+        else:
+            value = False
+    if value:
+        print("Yes")
     else:
-        return("Yes")
+        print("No")
 
 
 TEST = int(input())
 for lp_1 in range(TEST):
     ARR_STR = list(map(str, input().split()))
     permute(ARR_STR[0], ARR_STR[1])
-
-
-
-
