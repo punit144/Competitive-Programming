33790921fb97a793b761f7f20c2835df0fa8408c
Punit Grewal <pgrewal@learn.localdomain>
2019-11-12 18:37:16 +0530

    Time Complexity (Try and Except)

---
+++ Hacker-Earth/01_BasicsOfProgramming/two_number.py
@@ -0,0 +1,13 @@
+"""
+    Author: Punit Grewal
+    Year: 2019
+    Github: https://github.com/punit144/Competitive-Programming
+    Problem: HackerEarth <
+    Complexity:
+"""
+while True:
+    try:
+        VALA, VALB = list(map(int, input().split()))
+        print(VALA + VALB)
+    except EOFError:
+        break
