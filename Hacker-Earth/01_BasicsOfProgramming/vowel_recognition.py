210c44a587aa60e455a8cdd42129140849216a04
punit144 <punit144@gmail.com>
2019-11-13 01:00:06 +0530

    Big O Complexity

--- Hacker-Earth/01_BasicsOfProgramming/vowel_recognition.py
+++ Hacker-Earth/01_BasicsOfProgramming/vowel_recognition.py
@@ -5,59 +5,16 @@
     Problem: HackerEarth < Time Complexity
     Complexity: Medium
 """
-"""
-    Author: Punit Grewal
-    Year: 2019
-    Github: https://github.com/punit144/Competitive-Programming
-    Problem: HackerEarth < Time Complexity
-    Complexity: Medium
-"""
-"""
-    Author: Punit Grewal
-    Year: 2019
-    Github: https://github.com/punit144/Competitive-Programming
-    Problem: HackerEarth < Time Complexity
-    Complexity: Medium
-"""
 TEST = int(input())
-for lp_t in range(TEST):
+for lp_1 in range(TEST):
     STRING = input()
-    STRING = STRING.lower()
-    SUB_STRING = []
-    CTR = 0
-    CTR_N = len(STRING) - 1
-    IDX = 0
-    CTR_1 = 0
-    CTR_2 = 1
-    T_STR = ''
-    while 1:
-        if CTR_1 < CTR_2:
-            T_STR = T_STR + STRING[IDX]
-            IDX += 1
-            CTR_1 += 1
-        else:
-            if T_STR in SUB_STRING:
-                pass
-            else:
-                SUB_STRING.append(T_STR)
-            CTR_2 += 1
-            CTR_1 = 0
-            IDX = CTR
-            T_STR = ''
-            if CTR_2 == CTR_N+1:
-                CTR_1 = 0
-                IDX += 1
-                CTR += 1
-                CTR_2 = CTR_1 + 1
-                if CTR > 1:
-                    CTR_N -= 1
-        if CTR_N == 0:
-            SUB_STRING.append(STRING)
-            break
-    COMPARE = "aeiou"
+    COMPARE = "aeiouAEIOU"
+    LENGTH = len(STRING)
+    VOW_DIC = {}
+    for VOW_LOC, VOW_VAL in enumerate(STRING):
+        if VOW_VAL in COMPARE:
+            VOW_DIC[VOW_LOC] = VOW_VAL
     OUTPUT = 0
-    for lp_x in SUB_STRING:
-        for lp_vow in COMPARE:
-            if lp_vow in lp_x:
-                OUTPUT += 1
-    print(OUTPUT)
+    for lp_3 in VOW_DIC:
+        OUTPUT += (LENGTH-lp_3)*(lp_3+1)
+print(OUTPUT)
