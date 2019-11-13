8758c1691980b977cb4e4f3171fcbd892473526f
punit144 <punit144@gmail.com>
2019-11-14 02:02:50 +0530

    Program Solved

--- Hacker-Earth/01_BasicsOfProgramming/minimum_transanction.py
+++ Hacker-Earth/01_BasicsOfProgramming/minimum_transanction.py
@@ -5,32 +5,27 @@
     Problem: HackerEarth <
     Complexity:
 """
+"""
+    Author: Punit Grewal
+    Year: 2019
+    Github: https://github.com/punit144/Competitive-Programming
+    Problem: HackerEarth <
+    Complexity:
+"""
 N, M = map(int, input().split())
 LENTER = {}
-LEN_LST = []
-REN_LST = []
+OUTPUT = []
 
 for lp_g1 in range(M):
     U, V, W = map(int, input().split())
     if U in LENTER:
-        LENTER[U] -= W
-        OUTPUT += abs(w)
+        LENTER[U] = LENTER[U] - W
     else:
         LENTER[U] = -W
-        LEN_LST.append(U)
     if V in LENTER:
         LENTER[V] += W
     else:
         LENTER[V] = W
-        REN_LST.append(V)
+OUTPUT = sum(i for i in LENTER.values() if i < 0)
+print(abs(OUTPUT))
 
-OUTPUT = 0
-CTR = 0
-LENGTH = len(LEN_LST)
-while 1:
-    if LENTER[LEN_LST[CTR]] < 0:
-        OUTPUT += abs(LENTER[LEN_LST[CTR]])
-        CTR += 1
-        if CTR == LENGTH:
-            break
-print(OUTPUT)
