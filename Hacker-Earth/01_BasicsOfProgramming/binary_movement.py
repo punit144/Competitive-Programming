7550f1115aa0166874b26515cf3522f86fa1b958
Punit Grewal <pgrewal@learn.localdomain>
2019-11-14 23:12:51 +0530

    Binary Medium

---
+++ Hacker-Earth/01_BasicsOfProgramming/binary_movement.py
@@ -0,0 +1,15 @@
+"""
+    Author: Punit Grewal
+    Year: 2019
+    Github: https://github.com/punit144/Competitive-Programming
+    Problem: HackerEarth <
+    Complexity:
+"""
+ARR_LEN = int(input())
+ARR = list(map(int, input().split()))
+QRY = int(input())
+OUTPUT = 0
+for lp_g1 in range(QRY)
+    Q1, Q2 = int(input().split())
+    
+
