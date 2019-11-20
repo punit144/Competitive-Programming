30fe4ae23762924e18ade30eb09abbbaf4ef8dad
Punit Grewal <pgrewal@learn.localdomain>
2019-11-20 19:56:13 +0530

    New Problem Solution

--- Hacker-Earth/01_BasicsOfProgramming/binary_movement.py
+++ Hacker-Earth/01_BasicsOfProgramming/binary_movement.py
@@ -5,11 +5,21 @@
     Problem: HackerEarth <
     Complexity:
 """
-ARR_LEN = int(input())
+N = int(input())
 ARR = list(map(int, input().split()))
+BRR = [0] * (N+2)
 QRY = int(input())
-OUTPUT = 0
-for lp_g1 in range(QRY)
-    Q1, Q2 = int(input().split())
-    
 
+for lp_g1 in range(QRY):
+    Q1, Q2 = list(map(int, input().split()))
+    BRR[Q1] ^= 1
+    BRR[Q2+1] ^= 1
+
+for lp_g2 in range(1, N+1, 1):
+    BRR[lp_g2] ^= BRR[lp_g2-1]
+
+for lp_g4 in range(1, N+1, 1):
+    ARR[lp_g4-1] = ARR[lp_g4-1] ^ BRR[lp_g4]
+BRR.clear()
+print(sum(ARR))
+print(*ARR)
